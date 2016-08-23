68c675a67e16e63186a64d3ad5b967f7efa8326d
RealWaynebird <Ferdinand.Sedlmair@gmx.net>
2016-08-23 09:02:55 +0000

    zeugs geändert

--- rs/1mxwiki/folderforwarding/events.py
+++ rs/1mxwiki/folderforwarding/events.py
@@ -1,2 +1,7 @@
 def notifyFolderIsAdded(folder, event):
     folder.REQUEST.RESPONSE.redirect(folder.absolute_url() + "/++add++Document")
+
+def notifyDocumentIseAdded(document, event):
+    folder = aq_parent(document)
+    folder.setDefaultPage(document)    
+
